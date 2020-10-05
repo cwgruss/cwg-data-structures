6b76908c7cf2ac8d90828978090c7d56ef3e05c0
Carter Gruss <cwgruss@gmail.com>
2020-10-05 00:12:43 -0500

    test: Improve test coverage to 100%

--- packages/linked-list/__tests__/linked-list-node.spec.js
+++ packages/linked-list/__tests__/linked-list-node.spec.js
@@ -1,16 +1,29 @@
 import LinkedListNode from "../src/linked-list-node";
 
-describe('LinkedListNode', () => {
-    it('should create an initial node with NULL as a value', () => {
-        const node = new LinkedListNode(null);
-        expect(node.value).toBeNull();
-        expect(node.next).toBeNull();
+describe("LinkedListNode", () => {
+  it("should create an initial node with NULL as a value", () => {
+    const headNode = new LinkedListNode(null);
+    expect(headNode.value).toBeNull();
+    expect(headNode.next).toBeNull();
+  });
+
+  describe("toString", () => {
+    it("should return 'null' as a string when value is NULL", () => {
+      const headNode = new LinkedListNode(null);
+      const value = headNode.toString();
+      expect(typeof value).toBe("string");
+      expect(value).toBe("null");
     });
 
-    it('should return \'null\' when value is NULL', () => {
-        const node = new LinkedListNode(null);
-        const value = node.toString();
-        expect(typeof value).toBe('string');
-        expect(value).toBe('null');
+    it("should use callback function to create string response", () => {
+      const node = new LinkedListNode(null);
+      const toStringCallback = jest.fn((value) => {
+        return "--";
+      });
+      const value = node.toString(toStringCallback);
+      expect(typeof value).toBe("string");
+      expect(value).toBe("--");
+      expect(toStringCallback.mock.calls.length).toBe(1);
     });
+  });
 });
--- packages/linked-list/babel.config.js
+++ packages/linked-list/babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        modules: false,
         targets: {
           node: "current",
         },
