2481b372c1bedd6e077a0a7c3379931e2ba628cb
Carter Gruss <cwgruss@gmail.com>
2020-10-05 17:35:54 -0500

    quick commit

--- packages/linked-list/src/linked-list.js
+++ packages/linked-list/src/linked-list.js
@@ -1,3 +1,5 @@
+import LinkedListNode from "./linked-list-node.js";
+
 /**
  * A linked-list of nodes
  */
@@ -5,5 +7,28 @@ export default class linkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+  }
+
+  /**
+   * Add a new value to the end of the linked-list,
+   * returns the now updated linked-list
+   * @param {any} value
+   * @returns {LinkedList}
+   */
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    // If no head exists, create one
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    // Otherwise, append the new node to the end
+    // and make it the new tail
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
   }
 }
