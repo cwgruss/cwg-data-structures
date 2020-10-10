b12ed18baa41d1774ab64c1a17b2c4df73365cd8
Carter Gruss <cwgruss@gmail.com>
2020-10-10 12:38:12 -0500

    test: Test the optional param in the constructor

--- packages/linked-list/__tests__/node/linked-list-node.spec.js
+++ packages/linked-list/__tests__/node/linked-list-node.spec.js
@@ -1,29 +1,37 @@
-import LinkedListNode from "../../src/linked-list-node";
+import LinkedListNode from '../../src/linked-list-node';
 
-describe("LinkedListNode", () => {
-  it("should create an initial node with NULL as a value", () => {
-    const headNode = new LinkedListNode(null);
-    expect(headNode.value).toBeNull();
-    expect(headNode.next).toBeNull();
-  });
+describe('LinkedListNode', () => {
+	it('should create an initial node with NULL as a value', () => {
+		const headNode = new LinkedListNode(null);
+		expect(headNode.value).toBeNull();
+		expect(headNode.next).toBeNull();
+	});
 
-  describe("toString", () => {
-    it("should return 'null' as a string when value is NULL", () => {
-      const headNode = new LinkedListNode(null);
-      const value = headNode.toString();
-      expect(typeof value).toBe("string");
-      expect(value).toBe("null");
-    });
+	it('should create a new node pointed at the next node', () => {
+		const nextNode = new LinkedListNode(2);
+		const headNode = new LinkedListNode(1, nextNode);
 
-    it("should use callback function to create string response", () => {
-      const node = new LinkedListNode(null);
-      const toStringCallback = (value) => {
-        return "--";
-      };
-      const value = node.toString(toStringCallback);
-      expect(typeof value).toBe("string");
-      expect(value).toBe("--");
-      // expect(toStringCallback.mock.calls.length).toBe(1);
-    });
-  });
+		expect(headNode.value).toBe(1);
+		expect(headNode.next).toBe(nextNode);
+	});
+
+	describe('toString', () => {
+		it("should return 'null' as a string when value is NULL", () => {
+			const headNode = new LinkedListNode(null);
+			const value = headNode.toString();
+			expect(typeof value).toBe('string');
+			expect(value).toBe('null');
+		});
+
+		it('should use callback function to create string response', () => {
+			const node = new LinkedListNode(null);
+			const toStringCallback = (value) => {
+				return '--';
+			};
+			const value = node.toString(toStringCallback);
+			expect(typeof value).toBe('string');
+			expect(value).toBe('--');
+			// expect(toStringCallback.mock.calls.length).toBe(1);
+		});
+	});
 });
